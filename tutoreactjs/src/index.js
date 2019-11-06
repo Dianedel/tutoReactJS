d39d0021d789cbffd7c8438bef77245d00379598
Diane de Lassence <dianedelassence@gmail.com>
2019-11-06 18:25:18 +0100

    jeux morpion ok

--- tutoreactjs/src/index.js
+++ tutoreactjs/src/index.js
@@ -14,7 +14,6 @@ function Square(props) {
       );
     }
 
-
   class Board extends React.Component {
       constructor(props) {
           super(props);
@@ -24,8 +23,21 @@ function Square(props) {
           };
       }
 
+      render() {
+          const winner = calculateWinner(this.state.squares);
+          let status;
+          if (winner) {
+              status = winner = ' a gagné';
+          } else {
+              status = 'Next player'
+          }
+      }
+
       handleClick(i) {
           const squares = this.state.squares.slice();
+          if (calculateWinner(squares) || squares[i]) {
+              return;
+          }
           squares[i] = this.state.xIsNext ? 'X' : '0';
           this.setState({
               squares: squares,
@@ -43,7 +55,13 @@ function Square(props) {
     }
 
     render() {
-      const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+          let status;
+          if (winner) {
+              status = winner + ' a gagné';
+          } else {
+              status = 'Next player : ' + (this.state.xIsNext ? 'X' : '0');
+          }
 
       return (
         <div>
@@ -82,6 +100,26 @@ function Square(props) {
         </div>
       );
     }
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 
   // ========================================
